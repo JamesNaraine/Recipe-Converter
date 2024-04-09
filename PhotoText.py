2d54990ee3b51e28611b64d8dd65e9e67f7d8572
TheLukeShadow <xiaospook@gmail.com>
2024-04-09 21:52:55 +0200

    all of tesseract

--- PhotoText.py
+++ PhotoText.py
@@ -1,8 +1,11 @@
 from PIL import Image
 import pytesseract 
+
 # import tesseract 
-pytesseract.pytesseract.tesseract_cmd = r'<"C:\Program Files\Tesseract-OCR\tesseract.exe">'
-tessdata_dir_config = '--tessdata-dir "C:\Program Files\Tesseract-OCR\tessdata"'
+pytesseract.pytesseract.tesseract_cmd = r'<"C:\Users\Lucas\Desktop\New Stuff\VS\Recipe-Converter\Tesseract-OCR\tesseract.exe">'
+# pytesseract.pytesseract.tesseract_cmd = r'<C:\Users\Lucas\AppData\Local\Programs\Tesseract-OCR\tesseract.exe>'
+
+# tessdata_dir_config = '--tessdata-dir "C:\Program Files\Tesseract-OCR\tessdata"'
 
 image = Image.open(r"C:\Users\Lucas\Desktop\New Stuff\VS\Recipe-Converter\Images\20240409_122721.jpg")
 
