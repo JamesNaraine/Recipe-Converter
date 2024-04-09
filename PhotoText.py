6281d9da250a7019df9adacc41b7f4ed7c96094c
TheLukeShadow <xiaospook@gmail.com>
2024-04-09 22:06:29 +0200

    Functioning photo to text

--- PhotoText.py
+++ PhotoText.py
@@ -2,7 +2,8 @@ from PIL import Image
 import pytesseract 
 
 # import tesseract 
-pytesseract.pytesseract.tesseract_cmd = r'<C:\Users\Lucas\Desktop\New Stuff\VS\Recipe-Converter\Tesseract-OCR\tesseract.exe>'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Lucas\Desktop\New Stuff\VS\Recipe-Converter\Tesseract-OCR\tesseract.exe'
+
 # pytesseract.pytesseract.tesseract_cmd = r'<C:\Users\Lucas\AppData\Local\Programs\Tesseract-OCR\tesseract.exe>'
 
 # tessdata_dir_config = '--tessdata-dir "C:\Program Files\Tesseract-OCR\tessdata"'
